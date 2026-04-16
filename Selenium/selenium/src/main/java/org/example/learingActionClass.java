661f3e1b0a44d8dbfe7a8ef99cb7b23784148533
Arshad Beg <arshadbeg286@gmail.com>
2026-04-16 15:41:06 +0530

    learning Action class used Right click button

---
+++ Selenium/selenium/src/main/java/org/example/learingActionClass.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class learingActionClass {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+
+                try {
+
+                    driver.get("https://demoqa.com/buttons");
+                    driver.manage().window().maximize();
+
+                    // Wait setup
+                    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+                    // Locate element with wait
+                    WebElement rightBtn = wait.until(
+                            ExpectedConditions.elementToBeClickable(By.id("rightClickBtn"))
+                    );
+
+                    // Scroll to element (important)
+                    ((JavascriptExecutor) driver).executeScript(
+                            "arguments[0].scrollIntoView({block:'center'});", rightBtn
+                    );
+
+                    // Small pause (optional but helps stability)
+                    Thread.sleep(500);
+
+                    // Perform right click
+                    Actions actions = new Actions(driver);
+                    actions.moveToElement(rightBtn).contextClick().perform();
+
+                    System.out.println("Right click button success");
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
